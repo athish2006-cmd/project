89c6c2ac968a3740df34bf8356c14709e38df2a9
athish2006-cmd <athishj369@gmail.com>
2025-12-14 13:07:27 +0530

    Delete pract.py

--- pract.py
+++
@@ -1,8 +0,0 @@
-length = float(input(" enter the length of the rectiangle"))
-width = float(input("enter the width of the rectiangle"))
-
-area = length * width
-perimeter = 2 * (length + width)
-
-print(f"area of the rectangle:  {area}")
-print(f" perimeter of the rectangle:  {perimeter}")
